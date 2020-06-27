76313dd0e4c0d9bea3e37589db1a04e2cf48e32b
vcaruso <vinnycar0923@gmail.com>
2020-06-27 15:22:08 -0700

    added JS to print fetched data to page on search

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,6 @@ var currentWeatherHandler = function () {
     //append them children
 }
 
-var futureWeatherHandler = function () {
-    //append them children
-}
-
 var getLatLng = function () {
     var mapquestUrl = "http://www.mapquestapi.com/geocoding/v1/address?key=CGM5S6mK5h8rGeCXOD165GEL39leUoI7&location=" + cityName
     fetch(mapquestUrl)
@@ -37,21 +33,75 @@ var getWeather = function () {
             if (response.ok) {
                 response.json().then(function (data) {
                     //clear all divs
-                    console.log(data);
-                    //Current Info
+                    console.log(data)
                     var cityName = cityName
-                    var currentDate = moment.unix(data.current.dt).format("MMM Do YY")
+                    var currentDate = moment.unix(data.current.dt).format("l")
                     var currentClouds = parseInt(data.current.clouds)
                     var currentTemp = Math.floor(data.current.temp)
                     var currentHum = data.current.humidity
                     var currentWind = data.current.wind_speed
                     var currentUV = data.current.uvi
                     
-                    var futureDateOne = moment.unix(data.daily[0].dt).format("MMM Do YY")
-                    var futureCloudsOne = parseInt(data.daily[0].clouds)
-                    var futureTempOne = Math.floor(data.daily[0].temp)
-                    var futureHumOne = data.daily[0].humidity
                     
+                    $("#futureDateOne").text(moment.unix(data.daily[0].dt).format("l"));
+                    var futureCloudsOne = parseInt(data.daily[0].clouds)
+                    if (futureCloudsOne > 70) {
+                        // $("#futureIconOne") cloudy icon 
+                    } else if (69 > futureCloudsOne > 25) {
+                        // $("#futureIconOne") partly cloudy icon
+                    } else {
+                        // $("#futureIconOne") Sunny Icon
+                    }
+                    $("#futureTempOne").text("Temp: " + Math.floor((data.daily[0].temp.max + data.daily[0].temp.min) / 2));
+                    $("#futureHumOne").text("Hum: " + data.daily[0].humidity)
+                    
+                        $("#futureDateTwo").text(moment.unix(data.daily[1].dt).format("l"));
+                        var futureCloudsTwo = parseInt(data.daily[1].clouds)
+                        if (futureCloudsTwo > 70) {
+                            // $("#futureIconTwo") cloudy icon 
+                        } else if (69 > futureCloudsTwo > 25) {
+                            // $("#futureIconTwo") partly cloudy icon
+                        } else {
+                            // $("#futureIconTwo") Sunny Icon
+                        }
+                        $("#futureTempTwo").text("Temp: " + Math.floor((data.daily[1].temp.max + data.daily[1].temp.min) / 2));
+                        $("#futureHumTwo").text("Hum: " + data.daily[1].humidity)
+                    
+                    $("#futureDateThree").text(moment.unix(data.daily[2].dt).format("l"));
+                    var futureCloudsThree = parseInt(data.daily[2].clouds)
+                    if (futureCloudsThree > 70) {
+                        // $("#futureIconThree") cloudy icon 
+                    } else if (69 > futureCloudsThree > 25) {
+                        // $("#futureIconThree") partly cloudy icon
+                    } else {
+                        // $("#futureIconThree") Sunny Icon
+                    }
+                    $("#futureTempThree").text("Temp: " + Math.floor((data.daily[2].temp.max + data.daily[2].temp.min) / 2));
+                    $("#futureHumThree").text("Hum: " + data.daily[2].humidity)
+                    
+                        $("#futureDateFour").text(moment.unix(data.daily[3].dt).format("l"));
+                        var futureCloudsFour = parseInt(data.daily[3].clouds)
+                        if (futureCloudsFour > 70) {
+                            // $("#futureIconFour") cloudy icon 
+                        } else if (69 > futureCloudsFour > 25) {
+                            // $("#futureIconFour") partly cloudy icon
+                        } else {
+                            // $("#futureIconFour") Sunny Icon
+                        }
+                        $("#futureTempFour").text("Temp: " + Math.floor((data.daily[3].temp.max + data.daily[3].temp.min) / 2));
+                        $("#futureHumFour").text("Hum: " + data.daily[3].humidity)
+                    
+                    $("#futureDateFive").text(moment.unix(data.daily[4].dt).format("l"));
+                    var futureCloudsFive = parseInt(data.daily[4].clouds)
+                    if (futureCloudsFive > 70) {
+                        // $("#futureIconFive") cloudy icon 
+                    } else if (69 > futureCloudsFive > 25) {
+                        // $("#futureIconFive") partly cloudy icon
+                    } else {
+                        // $("#futureIconFive") Sunny Icon
+                    }
+                    $("#futureTempFive").text("Temp: " + Math.floor((data.daily[4].temp.max + data.daily[4].temp.min) / 2));
+                    $("#futureHumFive").text("Hum: " + data.daily[4].humidity)
                 })
             } else {
                 alert("Error: " + response.statusText);
@@ -68,7 +118,10 @@ $("#cityInputForm").submit(function () {
     event.preventDefault();
     cityName = $("#cityInput").val().trim();
     console.log(cityName);
+    //display main content
     getLatLng();
 })
 
 //Get city name from history
+
+//Hide main content on load
