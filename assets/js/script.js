13ba620631b5a7fbd34370c8fe5d528087890f34
vcaruso <vinnycar0923@gmail.com>
2020-06-27 13:07:02 -0700

    located necessary data from Open Weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,13 +23,27 @@ var getLatLng = function () {
 
 var getWeather = function () {
 
-    var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?" + latLng + "&exclude=hourly,minutely&appid=7402dbd1ea9b7f290130809de84fd4b5"
+    var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?" + latLng + "&exclude=hourly,minutely&appid=7402dbd1ea9b7f290130809de84fd4b5&units=imperial"
     
     fetch(weatherURL)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
                     console.log(data);
+                    //Current Info
+                    var cityName = cityName
+                    var currentDate = moment.unix(data.current.dt).format("MMM Do YY")
+                    var currentClouds = parseInt(data.current.clouds)
+                    var currentTemp = Math.floor(data.current.temp)
+                    var currentHum = data.current.humidity
+                    var currentWind = data.current.wind_speed
+                    var currentUV = data.current.uvi
+                    
+                    var futureDateOne = moment.unix(data.daily[0].dt).format("MMM Do YY")
+                    var futureCloudsOne = parseInt(data.daily[0].clouds)
+                    var futureTempOne = Math.floor(data.daily[0].temp)
+                    var futureHumOne = data.daily[0].humidity
+                    
                 })
             } else {
                 alert("Error: " + response.statusText);
