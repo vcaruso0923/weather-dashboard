0949f3030304d6a9fa70df2496c89589aca2ce63
vcaruso <vinnycar0923@gmail.com>
2020-07-09 15:43:24 -0700

    Changed Mapquest API Key

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var hideContent = function () {
 hideContent();
 
 var getLatLng = function () {
-    var mapquestUrl = "https://www.mapquestapi.com/geocoding/v1/address?key=CGM5S6mK5h8rGeCXOD165GEL39leUoI7&location=" + cityName
+    var mapquestUrl = "https://www.mapquestapi.com/geocoding/v1/address?key=oVyE3Ks9NWAfklACf3tDZUgJ3ztKqzfp&location=" + cityName
     fetch(mapquestUrl)
         .then(function (response) {
             if (response.ok) {
